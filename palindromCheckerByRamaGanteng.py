fc975d874604121fea4e6489d64ecad504ae5de4
= <=>
2023-10-26 18:17:13 -0700

    Initial Commit

---
+++ palindromCheckerByRamaGanteng.py
@@ -0,0 +1,20 @@
+def is_palindrome(word):
+    word = word.lower()
+    word = word.replace(" ", "")
+    reversedWord = word[::-1]
+
+    if word == reversedWord:
+        return True
+    else:
+        return False
+
+while True:
+    inputWord = input("Masukkan sebuah kata (atau ketik 'exit' untuk keluar): ")
+
+    if inputWord.lower() == 'exit':
+        break
+
+    if is_palindrome(inputWord):
+        print(f"'{inputWord}' adalah palindrom (True) dan memiliki {len(inputWord)} karakter.")
+    else:
+        print(f"'{inputWord}' bukan palindrom (False) dan memiliki {len(inputWord)} karakter.")
