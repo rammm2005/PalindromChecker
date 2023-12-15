a0a033a7a099bf86307046522864a95f351d6a9d
rammm2005 <ditarama985@gmail.com>
2023-12-15 10:58:05 +0700

    fix code on translation and search OBST

--- knapseck/knapseck.c
+++ knapseck/knapseck.c
@@ -1,57 +1,101 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-void knapsack(int n, int weights[], int values[], int capacity) {
-    int dp[n + 1][capacity + 1];
-
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= capacity; j++) {
-            if (i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if (weights[i - 1] <= j)
-                dp[i][j] = max(dp[i - 1][j], values[i - 1] + dp[i - 1][j - weights[i - 1]]);
-            else
-                dp[i][j] = dp[i - 1][j];
-        }
-    }
-
-    int selected_items[n];
-    int count = 0;
-    for (int i = n, j = capacity; i > 0 && j > 0; i--) {
-        if (dp[i][j] != dp[i - 1][j]) {
-            selected_items[count++] = i - 1;
-            j -= weights[i - 1];
-        }
-    }
-
-    printf("Maximum value: %d\nSelected items: ", dp[n][capacity]);
-    for (int i = count - 1; i >= 0; i--) {
-        printf("%d ", selected_items[i]);
-    }
-    printf("\n");
-}
+// Deklarasi fungsi knapsack
+void knapsack(int values[], int weights[], int n, int capacity, int *maxValue, int *totalWeight, int selectedItems[]);
 
 int main() {
     int n;
-    printf("Enter the number of items: ");
+
+    // Input jumlah barang
+    printf("Masukkan jumlah barang: ");
     scanf("%d", &n);
 
-    int weights[n];
-    int values[n];
-    int capacity;
+    // Alokasi memori untuk arrays values, weights, dan selectedItems
+    int *values = (int *)malloc(n * sizeof(int));
+    int *weights = (int *)malloc(n * sizeof(int));
+    int *selectedItems = (int *)malloc(n * sizeof(int));
 
-    for (int i = 0; i < n; i++) {
-        printf("Enter weight and value for item %d: ", i + 1);
-        scanf(" %d %d", &weights[i], &values[i]);  
+    if (values == NULL || weights == NULL || selectedItems == NULL) {
+        fprintf(stderr, "Gagal melakukan alokasi memori.\n");
+        return 1;
     }
 
-    printf("Enter the knapsack capacity: ");
+    // Input nilai dan berat masing-masing barang
+    printf("Masukkan nilai dan berat masing-masing barang:\n");
+    for (int i = 0; i < n; i++) {
+        printf("Barang %d:\n", i + 1);
+        printf("Nilai: ");
+        scanf("%d", &values[i]);
+        printf("Berat: ");
+        scanf("%d", &weights[i]);
+        selectedItems[i] = 0; // Awalnya, belum ada barang yang dipilih
+    }
+
+    int capacity;
+    // Input kapasitas knapsack
+    printf("Masukkan kapasitas knapsack: ");
     scanf("%d", &capacity);
 
-    knapsack(n, weights, values, capacity);
+    int maxValue, totalWeight;
+
+    // Panggil fungsi knapsack
+    knapsack(values, weights, n, capacity, &maxValue, &totalWeight, selectedItems);
+
+    // Tampilkan hasil
+    printf("Nilai maksimum yang dapat dimasukkan ke dalam knapsack: %d\n", maxValue);
+
+    // Tampilkan total berat barang yang dimasukkan ke dalam knapsack
+    printf("Total berat barang yang dimasukkan ke dalam knapsack: %d\n", totalWeight);
+
+    // Tampilkan kapasitas knapsack yang digunakan
+    printf("Kapasitas knapsack yang digunakan: %d\n", capacity - (capacity - totalWeight));
+
+    // Tampilkan barang-barang yang dimasukkan ke dalam knapsack
+    printf("Barang-barang yang dimasukkan ke dalam knapsack:\n");
+    for (int i = 0; i < n; i++) {
+        if (selectedItems[i] == 1) {
+            printf("Barang %d - Nilai: %d, Berat: %d\n", i + 1, values[i], weights[i]);
+        }
+    }
+
+    // Bebaskan memori yang dialokasikan
+    free(values);
+    free(weights);
+    free(selectedItems);
 
     return 0;
+}
+
+// Definisi fungsi knapsack
+void knapsack(int values[], int weights[], int n, int capacity, int *maxValue, int *totalWeight, int selectedItems[]) {
+    int i, w;
+    int dp[n + 1][capacity + 1];
+
+    // Mengisi tabel DP
+    for (i = 0; i <= n; i++) {
+        for (w = 0; w <= capacity; w++) {
+            if (i == 0 || w == 0)
+                dp[i][w] = 0;
+            else if (weights[i - 1] <= w)
+                dp[i][w] = (values[i - 1] + dp[i - 1][w - weights[i - 1]]) > dp[i - 1][w] ? (values[i - 1] + dp[i - 1][w - weights[i - 1]]) : dp[i - 1][w];
+            else
+                dp[i][w] = dp[i - 1][w];
+        }
+    }
+
+    // Nilai maksimum yang dapat dimasukkan ke dalam knapsack
+    *maxValue = dp[n][capacity];
+
+    // Menghitung total berat barang yang dimasukkan
+    *totalWeight = 0;
+    w = capacity;
+    for (i = n; i > 0 && *maxValue > 0; i--) {
+        if (*maxValue != dp[i - 1][w]) {
+            selectedItems[i - 1] = 1; // Menandai bahwa barang i dipilih
+            *totalWeight += weights[i - 1];
+            *maxValue -= values[i - 1];
+            w -= weights[i - 1];
+        }
+    }
 }
--- optimal-search-tree/test.c
+++ optimal-search-tree/test.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
-// Node structure for BST
 struct Node {
-    char key[50];
-    char value[50];
+    char key[100];
+    char value[100];
     struct Node* left;
     struct Node* right;
 };
 
-// Function to create a new Node
 struct Node* newNode(char key[], char value[]) {
     struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
-    strcpy(temp->key, key);
-    strcpy(temp->value, value);
-    temp->left = temp->right = NULL;
+    if (temp != NULL) {
+        strcpy(temp->key, key);
+        strcpy(temp->value, value);
+        temp->left = temp->right = NULL;
+    }
     return temp;
 }
 
-// Function to insert a new Node into BST
+// Updated insert function
 struct Node* insert(struct Node* root, char key[], char value[]) {
     if (root == NULL) {
         return newNode(key, value);
@@ -35,58 +36,12 @@ struct Node* insert(struct Node* root, char key[], char value[]) {
     return root;
 }
 
-struct Node* optimizeBST(struct Node* root, int* cost) {
-    if (root == NULL) {
-        return NULL;
-    }
-
-    // Perform DP on left and right subtrees
-    root->left = optimizeBST(root->left, cost);
-    root->right = optimizeBST(root->right, cost);
-
-    // Initialize cost to the value of the root
-    int currentCost = (root->left != NULL ? root->left->value[0] : 0) +
-                      (root->right != NULL ? root->right->key[0] : 0);
-
-    // Compare with the minimum cost so far
-    if (currentCost < *cost) {
-        *cost = currentCost;
-        return root;
-    }
-
-    // If the root is not the optimal root, try with the left and right children
-    struct Node* leftOptimal = (root->left != NULL) ? root->left->right : NULL;
-    struct Node* rightOptimal = (root->right != NULL) ? root->right->left : NULL;
-
-    // Check if changing the left child results in a lower cost
-    if (leftOptimal != NULL) {
-        int leftCost = leftOptimal->value[0] + (root->right != NULL ? root->right->key[0] : 0);
-        if (leftCost < *cost) {
-            *cost = leftCost;
-            return leftOptimal;
-        }
-    }
-
-    // Check if changing the right child results in a lower cost
-    if (rightOptimal != NULL) {
-        int rightCost = (root->left != NULL ? root->left->value[0] : 0) + rightOptimal->key[0];
-        if (rightCost < *cost) {
-            *cost = rightCost;
-            return rightOptimal;
-        }
-    }
-
-    return root;
-}
-
-
-// Function to perform search in optimized BST
 char* search(struct Node* root, char key[]) {
     while (root != NULL) {
-        int cmp = strcmp(key, root->key);
-        if (cmp == 0) {
+        // Jika kata kunci cocok dengan awalan pada node
+        if (strstr(root->key, key) == root->key) {
             return root->value; // Found the key
-        } else if (cmp < 0) {
+        } else if (strcmp(key, root->key) < 0) {
             root = root->left; // Search in the left subtree
         } else {
             root = root->right; // Search in the right subtree
@@ -96,48 +51,139 @@ char* search(struct Node* root, char key[]) {
     return "Not Found";
 }
 
-// Function to display the optimized BST
+
 void display(struct Node* root) {
     if (root != NULL) {
         display(root->left);
-        printf("%s: %s\n", root->key, root->value);
+        printf("%s : %s\n", root->key, root->value);
         display(root->right);
     }
+}
+
+int optimalCost(int freq[], int i, int j) {
+    int cost = 0;
+    for (int k = i; k <= j; k++) {
+        cost += freq[k];
+    }
+    return cost;
+}
+
+struct Node* constructOBSTUtil(char keys[][100], char values[][100], int freq[], int i, int j, int cost[][100], struct Node* dp[][100]) {
+    if (i > j) {
+        return NULL;
+    }
+
+    if (dp[i][j] != NULL) {
+        // Return the result if already computed
+        return dp[i][j];
+    }
+
+    int minCost = INT_MAX;
+    int rootIndex = -1;
+
+    for (int k = i; k <= j; k++) {
+        if (cost[i][k - 1] + cost[k + 1][j] < minCost) {
+            minCost = cost[i][k - 1] + cost[k + 1][j];
+            rootIndex = k;
+        }
+    }
+
+    freq[rootIndex]++;  // Update frequency for the selected key
+
+    struct Node* root = newNode(keys[rootIndex], values[rootIndex]);
+    root->left = constructOBSTUtil(keys, values, freq, i, rootIndex - 1, cost, dp);
+    root->right = constructOBSTUtil(keys, values, freq, rootIndex + 1, j, cost, dp);
+
+    dp[i][j] = root;  // Save the result in the DP table
+    return root;
+}
+
+struct Node* constructOBST(char keys[][100], char values[][100], int freq[], int n) {
+    int cost[n][n];
+    struct Node* dp[n][n];
+
+    memset(dp, 0, sizeof(dp));  // Initialize DP table with NULL
+
+    for (int i = 0; i < n; i++) {
+        cost[i][i] = freq[i];
+        dp[i][i] = newNode(keys[i], values[i]);  // Base case: single node
+    }
+
+    for (int L = 2; L <= n; L++) {
+        for (int i = 0; i <= n - L; i++) {
+            int j = i + L - 1;
+            cost[i][j] = INT_MAX;
+
+            for (int r = i; r <= j; r++) {
+                int c = ((r > i) ? cost[i][r - 1] : 0) +
+                        ((r < j) ? cost[r + 1][j] : 0) +
+                        optimalCost(freq, i, j);
+
+                if (c < cost[i][j]) {
+                    cost[i][j] = c;
+                }
+            }
+        }
+    }
+
+    return constructOBSTUtil(keys, values, freq, 0, n - 1, cost, dp);
 }
 
 int main() {
     struct Node* root = NULL;
     int n, i;
-    char key[50], value[50];
+    char entry[256];
+    char key[100], value[100];
 
     printf("Enter the number of entries in the dictionary: ");
     scanf("%d", &n);
+    getchar(); // Consume the newline character
+
+    int freq[n];
+    memset(freq, 0, sizeof(freq));
+
+    char keys[n][100];
+    char values[n][100];
 
     printf("Enter the dictionary entries (English word : Indonesian word):\n");
     for (i = 0; i < n; i++) {
-        scanf("%s %s", key, value);
+        fgets(entry, sizeof(entry), stdin);
+        sscanf(entry, "%99[^:]:%99[^\n]", key, value);
+
+        size_t len = strlen(value);
+        if (len > 0 && value[len - 1] == '\n') {
+            value[len - 1] = '\0';
+        }
+
+        strcpy(keys[i], key);
+        strcpy(values[i], value);
+
+        // Call the updated insert function
         root = insert(root, key, value);
     }
 
-    // Initialize cost to a large value
-    int cost = 999999;
-
-    // Optimize the BST using DP
-    root = optimizeBST(root, &cost);
-
-    printf("\nOptimized Binary Search Tree:\n");
+    printf("\nOptimal Binary Search Tree:\n");
     display(root);
 
-    // Perform searches in the optimized BST
     printf("\nEnter the English word to search (type 'exit' to stop): ");
     while (1) {
-        scanf("%s", key);
+        fgets(key, sizeof(key), stdin);
+
+        size_t len = strlen(key);
+        if (len > 0 && key[len - 1] == '\n') {
+            key[len - 1] = '\0';  // Remove the newline character
+        }
+
         if (strcmp(key, "exit") == 0) {
             break;
         }
 
+        printf("Searching for: %s\n", key);  // Add this line for debugging
+
         char* result = search(root, key);
         printf("Translation: %s\n", result);
+
+        printf("\nEnter the English word to search (type 'exit' to stop): ");
     }
 
     return 0;
